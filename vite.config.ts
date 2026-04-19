17e9a49ecab2bc319224789a2251233c5ba7f50e
BRMRs <ruizhengai@gmail.com>
2026-04-20 00:50:50 +0800

    修复部署路径到Climbuddy子目录

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/CPT208_C2-3/Climbuddy/',
   build: {
-    outDir: 'dist',
+    outDir: 'dist/Climbuddy',
     sourcemap: false,
   },
 });
